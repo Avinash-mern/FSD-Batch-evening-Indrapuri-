c8a51c155d7af392f68c901e12bbdb6599669f66
Avinash-mern <codeprepacademy123@gmail.com>
2024-12-04 18:55:23 +0530

    Create Scanner Input output.java

---
+++ Scanner I/Scanner Input output.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+
+class Main {
+    public static void main(String[] args) {
+        //frist create a object of scanner class
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter first number : ");
+        // int x = sc.nextInt();
+        // System.out.print("Enter second number : ");
+        // int y = sc.nextInt();
+        // int sum = x + y;
+        // System.out.print("Sum is : " + sum);
+        
+        
+        //for string
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your String : ");
+        String s = sc.nextLine(); //nextLine will take complete line
+        //next() will only print till first space;
+        System.out.print("Your string is : " + s);
+        
+    
+        //nextInt --> integer
+        //nextFloat --> float
+        //next() --> Print till first space
+        //nextLine() --> print complete line
+        
+        
+        
+    }
+}
